45d80178aff87c915221ee04683f448754e6acf5
vmishkovets <v.mishkovets@belitsoft.com>
2022-11-03 14:38:22 +0300

    fix bug with login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -23,11 +23,6 @@ class AuthController {
     const { email, password } = req.body;
     try {
       const user = await AuthService.login(email, password);
-      res.status(200).join(
-          {
-            message: 'Hello'
-          }
-      )
       return res.json(user);
     } catch (e) {
       next(e);
