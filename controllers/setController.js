00dcca1fc838714d8ba90f59880e9a65c7ed3f6a
vmishkovets <v.mishkovets@belitsoft.com>
2022-11-03 14:12:41 +0300

    sets list refactor

--- controllers/setController.js
+++ controllers/setController.js
@@ -24,63 +24,63 @@ class SetController {
     try {
       const { user_id, lang } = req.body;
       const setsList = await setService.getAllSets(user_id, lang);
-      let inactive_sets = [];
-      let first_repeat_sets = [];
-      let second_repeat_sets = [];
-      let third_repeat_sets = [];
-      let fourth_repeat_sets = [];
-      let fifth_repeat_sets = [];
-      let six_repeat_sets = [];
-      let sevens_repeat_sets = [];
-
-      setsList.forEach((item) => {
-        switch (item.learning_stage) {
-          case 0: {
-            inactive_sets = [...inactive_sets, item];
-            break;
-          }
-          case 1: {
-            first_repeat_sets = [...first_repeat_sets, item];
-            break;
-          }
-          case 2: {
-            second_repeat_sets = [...second_repeat_sets, item];
-            break;
-          }
-          case 3: {
-            third_repeat_sets = [...third_repeat_sets, item];
-            break;
-          }
-          case 4: {
-            fourth_repeat_sets = [...fourth_repeat_sets, item];
-            break;
-          }
-          case 5: {
-            fifth_repeat_sets = [...fifth_repeat_sets, item];
-            break;
-          }
-          case 6: {
-            six_repeat_sets = [...six_repeat_sets, item];
-            break;
-          }
-          case 7: {
-            sevens_repeat_sets = [...sevens_repeat_sets, item];
-            break;
-          }
-        }
-      });
-
-      const list = [
-        { title: "Repeat in 30 minutes", data: first_repeat_sets },
-        { title: "Repeat in 8 hours", data: second_repeat_sets },
-        { title: "Repeat in 1 day", data: third_repeat_sets },
-        { title: "Repeat in 3 days", data: fourth_repeat_sets },
-        { title: "Repeat in 10 days", data: fifth_repeat_sets },
-        { title: "Repeat in 1 month", data: six_repeat_sets },
-        { title: "Repeat in 3 month", data: sevens_repeat_sets },
-        { title: "Inactive", data: inactive_sets },
-      ].filter((item) => item.data.length !== 0);
-      return res.json(list);
+      // let inactive_sets = [];
+      // let first_repeat_sets = [];
+      // let second_repeat_sets = [];
+      // let third_repeat_sets = [];
+      // let fourth_repeat_sets = [];
+      // let fifth_repeat_sets = [];
+      // let six_repeat_sets = [];
+      // let sevens_repeat_sets = [];
+      //
+      // setsList.forEach((item) => {
+      //   switch (item.learning_stage) {
+      //     case 0: {
+      //       inactive_sets = [...inactive_sets, item];
+      //       break;
+      //     }
+      //     case 1: {
+      //       first_repeat_sets = [...first_repeat_sets, item];
+      //       break;
+      //     }
+      //     case 2: {
+      //       second_repeat_sets = [...second_repeat_sets, item];
+      //       break;
+      //     }
+      //     case 3: {
+      //       third_repeat_sets = [...third_repeat_sets, item];
+      //       break;
+      //     }
+      //     case 4: {
+      //       fourth_repeat_sets = [...fourth_repeat_sets, item];
+      //       break;
+      //     }
+      //     case 5: {
+      //       fifth_repeat_sets = [...fifth_repeat_sets, item];
+      //       break;
+      //     }
+      //     case 6: {
+      //       six_repeat_sets = [...six_repeat_sets, item];
+      //       break;
+      //     }
+      //     case 7: {
+      //       sevens_repeat_sets = [...sevens_repeat_sets, item];
+      //       break;
+      //     }
+      //   }
+      // });
+      //
+      // const list = [
+      //   { title: "Repeat in 30 minutes", data: first_repeat_sets },
+      //   { title: "Repeat in 8 hours", data: second_repeat_sets },
+      //   { title: "Repeat in 1 day", data: third_repeat_sets },
+      //   { title: "Repeat in 3 days", data: fourth_repeat_sets },
+      //   { title: "Repeat in 10 days", data: fifth_repeat_sets },
+      //   { title: "Repeat in 1 month", data: six_repeat_sets },
+      //   { title: "Repeat in 3 month", data: sevens_repeat_sets },
+      //   { title: "Inactive", data: inactive_sets },
+      // ].filter((item) => item.data.length !== 0);
+      return res.json(setsList);
     } catch (e) {
       next(e);
     }
