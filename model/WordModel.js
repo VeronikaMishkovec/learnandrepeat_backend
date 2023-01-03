727875a63c10b09309d039b501fd7698f76d7d98
vmishkovets <v.mishkovets@belitsoft.com>
2023-01-03 12:09:48 +0300

    feat: part of speech color identifier

--- model/WordModel.js
+++ model/WordModel.js
@@ -4,8 +4,8 @@ const WordSchema = new Schema({
   origin: { type: String, unique: true, required: true },
   translation: { type: String, required: true },
   set_id: { type: String },
-  part_of_speech: { type: String },
-  part_of_speech_color: { type: String },
+  part_of_speech: { type: String, required: true },
+  part_of_speech_color: { type: String, required: true },
 });
 
 module.exports = model("Word", WordSchema);
